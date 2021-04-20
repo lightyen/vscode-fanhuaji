ffc45fafea655b1f95dc085e03318dfb5ff33154
lightyen <lightyen0123@gmail.com>
2021-04-20 18:00:06 +0800

    prepare

--- client/extension.ts
+++ client/extension.ts
@@ -1,8 +1,22 @@
-import { NAME } from "shared"
 import vscode from "vscode"
 
 export async function activate(context: vscode.ExtensionContext) {
-	console.log(NAME)
+	const disposable = vscode.commands.registerCommand("extension.convert.helloworld", () => {
+		const editor = vscode.window.activeTextEditor
+		if (!editor) {
+			return
+		}
+
+		// const document = editor.document
+		const selection = editor.selection
+		// const word = document.getText(selection)
+
+		editor.edit(editBuilder => {
+			editBuilder.replace(selection, "helloworld")
+		})
+	})
+
+	context.subscriptions.push(disposable)
 }
 
 export async function deactivate() {
